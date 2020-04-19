c5b7f08710f10b04288f84071647a9f75510c227
shehan shaman <33275058+shehanshaman@users.noreply.github.com>
2020-04-19 21:14:17 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='G-Home',
+    version='',
+    packages=['GHome'],
+    url='',
+    license='',
+    author='shehan',
+    author_email='',
+    description=''
+)
